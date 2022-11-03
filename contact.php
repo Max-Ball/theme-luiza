9fcc761fdfbe6f97240baf302541e01d0eb2e466
Max Ball <max.ball3388@gmail.com>
2022-11-02 21:28:47 -0600

    setting email

--- contact.php
+++ contact.php
@@ -1,28 +1,6 @@
 <?php
-// PHP extensions Mail and Net_SMTP must be installed. If they are not already
-// installed, install them by PEAR:
-// > pear install Mail
-// > pear install Net_SMTP
-// SSL must be allowed. On Windows you should uncomment line in php.ini:
-//		extension=php_openssl.dll
-// If you use smtp.gmail.com as smpt host then use:
-//		https://accounts.google.com/DisplayUnlockCaptcha
-// and allow access for less secure apps:
-//		https://www.google.com/settings/security/lesssecureapps
-//		
-// =============================================================================
-// NOTES
-// contact.php – is a script for a web servers with php support.
-// Extensions which required for this script are often installed by default.
-// If they are not already installed, the easiest way to do this using PEAR.
-// Learn more about PEAR http://pear.php.net/.
-// Editing the php.ini required only on Windows. On other operating systems this
-// is usually not required. Location of this file depends on settings of web
-// server.
-// Also, your web server should not be denied access to external mail servers.
-// On the majority of paid and on many free hostings (eg OpenShift:
-// https://www.openshift.com/) all these features are available. 
-// =============================================================================
+
+
 
 require_once "Mail.php";
 // Change this options:
@@ -61,4 +39,3 @@ if (PEAR::isError($mail)) {
 } else {
 	echo($successMessage);
 }
-?>
