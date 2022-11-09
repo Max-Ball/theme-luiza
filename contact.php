af6f4637a8a61e561d11d6cfe14f234697a0d8aa
Max Ball <max.ball3388@gmail.com>
2022-11-09 14:57:18 -0700

    phpmailer

--- contact.php
+++ contact.php
@@ -1,41 +1,46 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-require_once "Mail.php";
-// Change this options:
-$username = 'user@gmail.com';
-$password = 'password';
-$smtpHost = 'ssl://smtp.gmail.com';
-$smtpPort = '465';
-$to = 'max.palmer.ball@gmail.com';
-$from = 'user@gmail.com';
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-$subject = 'Contact From Portfolio';
-$successMessage = 'Message sent!';
+try {
+	//Server settings
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'user@example.com';                     //SMTP username
+	$mail->Password   = 'secret';                               //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-$replyTo = $_POST['your-email'];
-$name = $_POST['your-name'];
-$body = $_POST['your-message'];
+	//Recipients
+	$mail->setFrom('from@example.com', 'Mailer');
+	$mail->addAddress('max.palmer.ball@gmail.com', 'Max Ball');     //Add a recipient
+	$mail->addAddress('ellen@example.com');               //Name is optional
+	$mail->addReplyTo('info@example.com', 'Information');
+	$mail->addCC('cc@example.com');
+	$mail->addBCC('bcc@example.com');
 
-$headers = array(
-	'From' => $name . " <" . $from . ">",
-	'Reply-To' => $name . " <" . $replyTo . ">",
-	'To' => $to,
-	'Subject' => $subject
-);
-$smtp = Mail::factory('smtp', array(
-	'host' => $smtpHost,
-	'port' => $smtpPort,
-	'auth' => true,
-	'username' => $username,
-	'password' => $password
-));
+	//Attachments
+	$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+	$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-$mail = $smtp->send($to, $headers, $body);
+	//Content
+	$mail->isHTML(true);                                  //Set email format to HTML
+	$mail->Subject = 'Here is the subject';
+	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-if (PEAR::isError($mail)) {
-	echo ($mail->getMessage());
-} else {
-	echo ($successMessage);
+	$mail->send();
+	echo 'Message has been sent';
+} catch (Exception $e) {
+	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
