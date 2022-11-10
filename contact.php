9525b5ba4463f517b75f607c8d9e30edfb61a44d
Max Ball <max.ball3388@gmail.com>
2022-11-09 17:05:07 -0700

    gitignore added

--- contact.php
+++ contact.php
@@ -17,7 +17,7 @@ try {
 	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 	$mail->Username   = 'max.palmer.ball@gmail.com';                     //SMTP username
-	$mail->Password   = 'dxjynlbxyqfoaylr';                               //SMTP password
+	$mail->Password   = 'zstjweukiqpoeoay';                               //SMTP password
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
 	$mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
